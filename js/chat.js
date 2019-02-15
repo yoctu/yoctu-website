5691c8a2eafa09707e8e21bdec22b44a4a5d1011
LaV <lav@yoctu.com>
2019-02-15 18:39:39 -0500

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -93,7 +93,7 @@ function writeToScreen(message) {
         var pre = document.createElement("p");
         let privmsgbody = "";
         chan = privmsg[2];
-        console.log(chan)
+        console.log(privmsg)
         for(var i = 3; i < privmsg.length; i++){
             privmsgbody += " " + privmsg[i];
         }

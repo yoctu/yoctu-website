f3af81dbe15c56c26756f45d329f7f8316b8cbd9
LaV <lav@yoctu.com>
2019-02-15 15:56:12 -0500

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -21,7 +21,7 @@ function init() {
     wsUri = document.querySelector('#yoctu-chat').dataset.url;
     chan = document.querySelector('#yoctu-chat').dataset.chan;
     color = document.querySelector('#yoctu-chat').dataset.color;
-    root.style.setProperty('--main-color', color);
+    document.querySelector(':root').style.setProperty('--main-color', color);
     websocket = new WebSocket(wsUri);
     websocket.onopen = function(evt) { onOpen(evt) };
     websocket.onclose = function(evt) { onClose(evt) };

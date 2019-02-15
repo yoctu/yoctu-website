2decbb9678da30a24307b0c06a57bd703c744ef5
LaV <lav@yoctu.com>
2019-02-15 12:25:07 -0500

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,7 +1,7 @@
 function openForm() {
-  document.getElementById("myForm").style.display = "block";
+  document.getElementById("chat-popup-form").style.display = "block";
 }
 
 function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+  document.getElementById("chat-popup-form").style.display = "none";
 }

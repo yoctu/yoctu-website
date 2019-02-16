fb6f72a5a2146e58c546c933fe8e55587be928cb
LaV <lav@yoctu.com>
2019-02-15 19:03:13 -0500

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -90,7 +90,7 @@ function writeToScreen(message) {
     privmsg = message.split(" ");
     msgdate = new  Date();
     output = document.getElementById("chat-messages-container");
-    if (privmsg[1] == "PRIVMSG" && output && !privmsg[3].startsWith(':***') && !privmsg[0].split("!")[0].substring(1).startsWith('#')) {
+    if (privmsg[1] == "PRIVMSG" && output && !privmsg[3].startsWith(':***') && !privmsg[0].split("!")[0].startsWith('#')) {
         var pre = document.createElement("p");
         let privmsgbody = "";
         chan = privmsg[0].split("!")[0].substring(1);

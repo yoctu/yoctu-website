ece9a63722a981334bd096ba8fce379bbffde6ee
LaV <lav@yoctu.com>
2019-03-03 20:40:42 -0500

    Update solr.js

--- js/solr.js
+++ js/solr.js
@@ -11,6 +11,12 @@ if (urlParams.has('entity')) {
     urlEntity = urlParams.get('entity');
 }
 
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113891182-4"></script>');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-113891182-4');
+
 jQuery(document).ready(function($) {
 
     var user = entities[urlEntity]["user"];

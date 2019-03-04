18dec2ecc6be6301bd50acd141f5460bf3a61f58
LaV <lav@yoctu.com>
2019-03-03 21:36:46 -0500

    Update solr.js

--- js/solr.js
+++ js/solr.js
@@ -64,9 +64,8 @@ jQuery(document).ready(function($) {
          "url": host + "/select?q="+query[0]+":"+query[1]+"&wt=json&rows=" + rows + "&start=" + start + "&sort="+columns[order[0][0]].data+"%20"+order[0][1],
          "dataType": "jsonp",
          "jsonp":"json.wrf",
-	 "headers": {
-            "Authorization": "Basic " + btoa(user+':'+password) 
-	 },
+	 "username": user,
+         "password": password,
 	/*"beforeSend": function(xhr){
             xhr.setRequestHeader("Authorization", "Basic " + btoa(user+':'+password));
          },*/

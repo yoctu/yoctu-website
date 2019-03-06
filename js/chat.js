d766b603ad49fbfb9b25862ac37fbac99ca35d6d
LaV <lav@yoctu.com>
2019-03-06 14:08:30 -0500

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -115,6 +115,13 @@ function writeToScreen(message) {
         msgnum += 1;
         cleanMessage();
         document.getElementById("chat-popup-form").style.display = "block";
+    } else {
+            console.log(privmsg[3]);
+           if (privmsg[3].startsWith('JOIN :You have not registered') {
+               sleep(500).then(() => {
+                   doSend("JOIN " + chan);
+               }
+           }
     }
 }
 

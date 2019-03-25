030eadc8a1d02d4b169d4522d8893e5f3b96c15e
Hicham Laasri <mail@klh-competence.com>
2019-03-25 22:17:22 +0100

    Update adresses.js
    add google address parser

--- js/parcel/addresses.js
+++ js/parcel/addresses.js
@@ -1,6 +1,6 @@
 var From = {
-    company:'' ,
-    name:'',
+    company: '',
+    name: '',
     phone: '',
     email: '',
     address1: '',
@@ -14,8 +14,8 @@ var From = {
 
 
 var To = {
-    company:'' ,
-    name:'',
+    company: '',
+    name: '',
     phone: '',
     email: '',
     address1: '',
@@ -28,90 +28,89 @@ var To = {
 };
 
 function initMap() {
-        var fromAddr = document.getElementById('form-from');
-        var toAddr = document.getElementById('form-to');
-        var autocompleteFrom = new google.maps.places.Autocomplete(fromAddr);
-        var autocompleteTo = new google.maps.places.Autocomplete(toAddr);
+    var fromAddr = document.getElementById('form-from');
+    var toAddr = document.getElementById('form-to');
+    var autocompleteFrom = new google.maps.places.Autocomplete(fromAddr);
+    var autocompleteTo = new google.maps.places.Autocomplete(toAddr);
 
-        autocompleteTo.addListener('place_changed', function() {
-          var placeTo = autocompleteTo.getPlace();
-          if (!placeTo.geometry) {
+    autocompleteTo.addListener('place_changed', function () {
+        var placeTo = autocompleteTo.getPlace();
+        if (!placeTo.geometry) {
             return;
-          }
+        }
 
-          var addressTo = '';
-          if (placeTo.address_components) {
+        var addressTo = '';
+        if (placeTo.address_components) {
             addressTo = [
-              (placeTo.address_components[0] && placeTo.address_components[0].short_name || ''),
-              (placeTo.address_components[1] && placeTo.address_components[1].short_name || ''),
-              (placeTo.address_components[2] && placeTo.address_components[2].short_name || '')
+                (placeTo.address_components[0] && placeTo.address_components[0].short_name || ''),
+                (placeTo.address_components[1] && placeTo.address_components[1].short_name || ''),
+                (placeTo.address_components[2] && placeTo.address_components[2].short_name || '')
             ].join(' ');
 
-            To.city = placeTo.address_components[2].short_name;
-            filtered_array = placeTo.address_components.filter(function(address_component){
-                return address_component.types.includes("postal_code");
-            });
-            To.postcode = filtered_array[0].short_name;
-            filtered_array = placeTo.address_components.filter(function(address_component){
-                return address_component.types.includes("country");
-            });
-            To.country_code = filtered_array[0].short_name;
-            To.address1 = placeTo.address_components[0].short_name + ' ' + placeTo.address_components[1].short_name;
-            fillForm(To,'to');
-          }
-        });
+            parseGoogleAdresse(placeTo, To);
+        }
+    });
 
 
-        autocompleteFrom.addListener('place_changed', function() {
-          var placeFrom = autocompleteFrom.getPlace();
-          if (!placeFrom.geometry) {
+    autocompleteFrom.addListener('place_changed', function () {
+        var placeFrom = autocompleteFrom.getPlace();
+        if (!placeFrom.geometry) {
             return;
-          }
+        }
 
-          var addressFrom = '';
-          if (placeFrom.address_components) {
+        var addressFrom = '';
+        if (placeFrom.address_components) {
             addressFrom = [
-              (placeFrom.address_components[0] && placeFrom.address_components[0].short_name || ''),
-              (placeFrom.address_components[1] && placeFrom.address_components[1].short_name || ''),
-              (placeFrom.address_components[2] && placeFrom.address_components[2].short_name || '')
+                (placeFrom.address_components[0] && placeFrom.address_components[0].short_name || ''),
+                (placeFrom.address_components[1] && placeFrom.address_components[1].short_name || ''),
+                (placeFrom.address_components[2] && placeFrom.address_components[2].short_name || '')
             ].join(' ');
+            parseGoogleAdresse(placeFrom, From);
+        }
+    });
+}
 
-            From.city = placeFrom.address_components[2].short_name;
-            filtered_array = placeFrom.address_components.filter(function(address_component){
-                return address_component.types.includes("postal_code");
-            });
-            From.postcode = filtered_array[0].short_name;
-            filtered_array = placeFrom.address_components.filter(function(address_component){
-                return address_component.types.includes("country");
-            });
-            From.country_code = filtered_array[0].short_name;
-            From.address1 = placeFrom.address_components[0].short_name + ' ' + placeFrom.address_components[1].short_name;
-              fillForm(From,'from');
-          }
-        });
-
-      }
-
-var LdapTool = function () {};
-LdapTool.prototype.getList = function() {
+var LdapTool = function () {
+};
+LdapTool.prototype.getList = function () {
     if (username === "") return this;
     $.ajax({
-        url:'/addresses',
+        url: '/addresses',
         dataType: 'json'
-    }).done(function(addresses) {
+    }).done(function (addresses) {
         addresses = addresses.response.docs;
         for (var address in addresses) {
-                $('#mytable').append(
-                    '<tr>'+
-                    '<td>'+
-                    '<input type="button" class="btn btn-sm btn-default useme" value="select"/></td>'+
-                    '<td>'+addresses[address].firstname+'</td>'+
-                    '<td>'+addresses[address].lastname+'</td>'+
-                    '<td class="address">'+addresses[address].addresse+'</td>'+
-                    '</tr>'
-                );
+            $('#mytable').append(
+                '<tr>' +
+                '<td>' +
+                '<input type="button" class="btn btn-sm btn-default useme" value="select"/></td>' +
+                '<td>' + addresses[address].firstname + '</td>' +
+                '<td>' + addresses[address].lastname + '</td>' +
+                '<td class="address">' + addresses[address].addresse + '</td>' +
+                '</tr>'
+            );
         }
     });
     this.name = name;
     return this;
 };
+
+var parseGoogleAdresse = function (googleAdresse, target) {
+    for (var i = 0; i < googleAdresse.length; i++) {
+        switch (googleAdresse.address_components[i].type[0]) {
+            case 'postal_code':
+                target.postcode = googleAdresse.address_components[i].short_name;
+                break;
+            case 'route':
+                target.address1 = googleAdresse.address_components[i].short_name;
+                break;
+            case 'locality':
+                break;
+            case '"country"':
+                target.country_code = googleAdresse.address_components[i].short_name;
+                break;
+            default:
+        }
+    }
+};
+

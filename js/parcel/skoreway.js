71baa51d9abd3647f24fa1598f47a54e0141cdf9
LaV <lav@yoctu.com>
2019-03-29 16:58:07 -0400

    Update skoreway.js

--- js/parcel/skoreway.js
+++ js/parcel/skoreway.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function () {
 $('#skoreway-shaq').on('click', function() {
     let dt = $('#datepicker').datepicker('getDate');
     var id = uuidv4();
-    var key = uuidv4().substring(0,8);
+    var key = uuidv4().substring(0,18).replace(/-/g,'');
     var data = { deContact: [ To.company, To.name, To.email, To.phone ], deDate: dt.getFullYear() + '-' + dayMonth(dt) + '-' + dt.getDate() + 'T' + dt.getHours() + ':' + dt.getMinutes() + ':00.000Z', dePlace: [ To.address1, To.postcode, To.city, To.country_code, To.country_code], dimension: [ parseFloat($('#form-length').val()), parseFloat($('#form-width').val()), parseFloat($('#form-height').val()), parseFloat($('#form-weight').val())], distance: "500", id: id, key: key, name: key, notes: "Nothing", puContact: [ From.company, From.name, From.email, From.phone ], puDate: "NOW+2HOUR", puPlace: [ From.address1, From.postcode, From.city, From.country_code, From.country_code ], reported_at: "NOW", source: [ usercode ], sourceName: [ usercode + " Demo" ], stackable: "No", status: "running", target: ["RE10000","RE10001","10HAR"], targetName: ["Simonin Inc.", "Houin Transport.","Taxi Henrion"], type: "auction", valid_from: "NOW+1HOUR", valid_until: "NOW+1DAY", transport: [ "LAMBDA" ], currency: "EUR" };
     $.ajax( {
         url: shaqurl + '/api/shaq/'+usercode,

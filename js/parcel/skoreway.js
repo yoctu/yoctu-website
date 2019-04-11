84f18b4752bc64a42af12ab70106c649055cf800
LaV <lav@yoctu.com>
2019-04-11 09:53:04 -0400

    Update skoreway.js

--- js/parcel/skoreway.js
+++ js/parcel/skoreway.js
@@ -7,11 +7,11 @@ function uuidv4() {
 
 jQuery(document).ready(function () {
 
-  $('#skoreway-shaq').on('click', function() {
+$('#skoreway-shaq').on('click', function() {
     let dt = $('#datepicker').datepicker('getDate');
     var id = uuidv4();
     var key = uuidv4().substring(0,18).replace(/-/g,'');
-    var data = { deContact: [ To.company, To.name, To.email, To.phone ], deDate: dt.getFullYear() + '-' + dayMonth(dt) + '-' + dt.getDate() + 'T' + dt.getHours() + ':' + dt.getMinutes() + ':00.000Z', dePlace: [ To.address1, To.postcode, To.city, To.country_code, To.country_code], dimension: [ "1", parseFloat($('#form-length').val()), parseFloat($('#form-width').val()), parseFloat($('#form-height').val()), parseFloat($('#form-weight').val()), "no"], distance: "500", id: id, key: key, name: key, notes: "Nothing", puContact: [ From.company, From.name, From.email, From.phone ], puDate: "NOW+2HOUR", puPlace: [ From.address1, From.postcode, From.city, From.country_code, From.country_code ], reported_at: "NOW", source: [ usercode ], sourceName: [ usercode + " Demo" ], sourceEmail: ["lav@yoctu.com"], stackable: "No", status: "running", target: ["82SKOREX"], targetName: ["Skore Express"], targetEmail: ["lav@yoctu.com"], type: "auction", visible: "public", valid_from: "NOW+1HOUR", valid_until: "NOW+1DAY", transport: "LAMBDA", currency: "EUR" };
+    var data = { deContact: [ To.company, To.name, To.email, To.phone ], deDate: dt.getFullYear() + '-' + dayMonth(dt) + '-' + dt.getDate() + 'T' + dt.getHours() + ':' + dt.getMinutes() + ':00.000Z', dePlace: [ To.address1, To.postcode, To.city, To.country_code, To.country_code], dimension: [ "1", parseFloat($('#form-length').val()), parseFloat($('#form-width').val()), parseFloat($('#form-height').val()), parseFloat($('#form-weight').val()), "no"], distance: "500", id: id, key: key, name: key, notes: "Nothing", puContact: [ From.company, From.name, From.email, From.phone ], puDate: "NOW+2HOUR", puPlace: [ From.address1, From.postcode, From.city, From.country_code, From.country_code ], reported_at: "NOW", source: [ usercode ], sourceName: [ usercode + " Demo" ], stackable: "No", status: "running", target: ["RE10000","RE10001","10HAR"], targetName: ["Simonin Inc.", "Houin Transport.","Taxi Henrion"], type: "auction", valid_from: "NOW+1HOUR", valid_until: "NOW+1DAY", transport: "LAMBDA", currency: "EUR" };
     $.ajax( {
         url: shaqurl + '/api/shaq/'+usercode,
         dataType: "json",
@@ -23,8 +23,9 @@ jQuery(document).ready(function () {
             xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + apikey));
         },
         complete: function (shaq) {
-            window.open(shaqurl + "/" + usercode + "/display/" + key,'_blank');
+            window.open(shaqurl + "/"+usercode+"/display/"+key,'_blank');
         }
     });
-  });
+});
+
 });

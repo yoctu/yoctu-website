3e7ec551e26c616a87a8b9c5d0b8e01ccb16bd22
Matteo Tirapelle <matteotirapelle@Matteos-MacBook-Air.local>
2019-06-05 15:03:38 -0400

    pluged in ga script edited 3plsoltions with the tms section

---
+++ shaq/lib/bootstrap/js/googleanalytics.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-135283327-3"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+
+ gtag('config', 'UA-135283327-3');
+</script>

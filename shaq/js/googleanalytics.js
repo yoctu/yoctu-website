7b46d46d9ad73dfd58a3cfa9579cde961c2fe04f
Matteo Tirapelle <matteotirapelle@Matteos-MacBook-Air.local>
2019-06-06 18:01:40 -0400

    small changes product team menu responsiveness

--- shaq/js/googleanalytics.js
+++ shaq/js/googleanalytics.js
@@ -1,8 +1,6 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-135283327-3"></script>
-<script>
+
  window.dataLayer = window.dataLayer || [];
  function gtag(){dataLayer.push(arguments);}
  gtag('js', new Date());
 
  gtag('config', 'UA-135283327-3');
-</script>

26111905eb1cbf9632f70d92f999339f84722cb5
git <git@yoctu.com>
2019-06-18 12:01:46 -0400

    remove plants

--- shaq/js/qs.slider.init.js
+++ shaq/js/qs.slider.init.js
@@ -1,36 +1,14 @@
 var configPricing = {
-	PriceBaseS :	'0',
-	PriceBaseM :	'10',
-	PriceBaseL :	'50',
-	PriceBaseXL :	'100',
 	PriceSHAQS :	'0.01',
-	PricePLANTS :	'2.00',
-	PriceUSERS :	'1.00',
+	PriceUSERS :	'1.10',
 	MaxSHAQS	: '5000',
-	MaxPLANTS	: '50',
 	MaxUSERS	: '500',
 	discount	: '0.25',
 }
 var presetspec = {
-	s: {
+	default: {
 		shaqs: "200",
-		plants: "2",
 		users: "5"
-	},
-	m: {
-		shaqs: "500",
-		plants: "3",
-		users: "10"
-	},
-	l: {
-		shaqs: "1000",
-		plants: "5",
-		users: "50"
-	},
-	xl: {
-		shaqs: "3000",
-		plants: "10",
-		users: "100"
 	}
 };
 
--- shaq/js/qs.slider.js
+++ shaq/js/qs.slider.js
@@ -18,8 +18,6 @@
                 slide: function(event, ui) {
                   if (id == 'shaqs') {
                     ISpec.setSHAQS(ui.value);
-                  } else if (id == 'plants') {
-                    ISpec.setPLANTS(ui.value);
                   } else if (id == 'users') {
                     ISpec.setUSERS(ui.value);
                   }
@@ -57,56 +55,12 @@
               ISpec.updatePrice();
             }
           });
-
-					$("#sPrice").html('$'+configPricing.PriceBaseS+'<span> Monthly</span>');
-					$("#mPrice").html('$'+configPricing.PriceBaseM+'<span> Monthly</span>');
-					$("#lPrice").html('$'+configPricing.PriceBaseL+'<span> Monthly</span>');
-					$("#xlPrice").html('$'+configPricing.PriceBaseXL+'<span> Monthly</span>');
-
-          var presetNames = new Array;
-          presetNames[0] = "s";
-          presetNames[1] = "m";
-          presetNames[2] = "l";
-          presetNames[3] = "xl";
-          $("#sPlan").on('click', function(e) {
-            e.preventDefault();
-						$("#planvalue").text(configPricing.PriceBaseS);
-            ISpec.selectPreset("s");
-						$(".plan-inner").css("border","1px solid #28a745");
-						$("#plan-inner-s").css("border","5px solid #28a745");
-          });
-          $("#mPlan").on('click', function(e) {
-            e.preventDefault();
-						$("#planvalue").text(configPricing.PriceBaseM);
-						ISpec.selectPreset("m");
-						$(".plan-inner").css("border","1px solid #28a745");
-						$("#plan-inner-m").css("border","5px solid #28a745");
-          });
-          $("#lPlan").on('click', function(e) {
-            e.preventDefault();
-						$("#planvalue").text(configPricing.PriceBaseL);
-            ISpec.selectPreset("l");
-						$(".plan-inner").css("border","1px solid #28a745");
-						$("#plan-inner-l").css("border","5px solid #28a745");
-          });
-          $("#xlPlan").on('click', function(e) {
-            e.preventDefault();
-						$("#planvalue").text(configPricing.PriceBaseXL);
-            ISpec.selectPreset("xl");
-						$(".plan-inner").css("border","1px solid #28a745");
-						$("#plan-inner-xl").css("border","5px solid #28a745");
-          });
         });
 
         var qsSlider = function() {
           var shaqsslider = {
             min: 1,
             max: o.MaxSHAQS,
-            step: 1
-          };
-          var plantsslider = {
-            min: 1,
-            max: o.MaxPLANTS,
             step: 1
           };
           var usersslider = {
@@ -117,13 +71,11 @@
 
           this.specification = {
             shaqs: shaqsslider,
-            plants: plantsslider,
             users: usersslider
           };
           var pricespec = {
             baseprice: o.PriceBaseS,
             shaqsSpec: o.PriceSHAQS,
-            plantsSpec: o.PricePLANTS,
             usersSpec: o.PriceUSERS
           };
 
@@ -132,12 +84,6 @@
             var sTotal = sliderStep + " " + units;
             $("div.values div#shaqsvalue").text(sTotal);
             $("div#qsSlider div#QsControls div#shaqs").slider("value", sliderStep);
-          }
-          this.setPLANTS = function(sliderStep) {
-            var units = 'PLANT(s)';
-            var sTotal = sliderStep + " " + units;
-            $("div.values div#plantsvalue").text(sTotal);
-            $("div#qsSlider div#QsControls div#plants").slider("value", sliderStep);
           }
           this.setUSERS = function(sliderStep) {
             var units = 'USER(s)';
@@ -149,7 +95,6 @@
           this.selectPreset = function(presetName) {
             var presetData = $(presetspec).attr(presetName);
             this.setSHAQS(presetData.shaqs);
-            this.setPLANTS(presetData.plants);
             this.setUSERS(presetData.users);
             this.updatePrice();
           };
@@ -164,15 +109,10 @@
           var calculatePrice = function() {
             var price = parseFloat($("#planvalue").text());
             var shaqs = parseFloat($("div.values div#shaqsvalue").text());
-            var plants = parseFloat($("div.values div#plantsvalue").text());
             var users = parseFloat($("div.values div#usersvalue").text());
             var period_is_year = parseInt($("div#periodselector div#period").slider("value"));
-						if (price > 0) {
-							price = price + (shaqs / 100) + plants + users;
-							$("#selectPricing").show();
-						} else {
-							$("#selectPricing").hide();
-						}
+						price = price + (shaqs / 100) users;
+ 					  $("#selectPricing").show();
             if (period_is_year) {
               price *= 12;
               price = price - (price * o.discount);
@@ -192,10 +132,8 @@
             for (i in presetspec) {
               var presetData = presetspec[i];
               var sliderSHAQS = $("div#QsControls div#shaqs").slider("value");
-              var sliderPLANTS = $("div#QsControls div#plants").slider("value");
               var sliderUSERS = $("div#QsControls div#users").slider("value");
               if (presetData.shaqs == sliderSHAQS &&
-                presetData.plants == sliderPLANTS &&
                 presetData.users == sliderUSERS) {
                 var presetClass = "preset" + i.toLowerCase();
                 $("div#presets div.product .btn").removeClass('btn-primary');

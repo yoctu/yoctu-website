ae25007597826a5fb16ebeb427b9426eeb2cc329
lav <lvenier@yoctu.com>
2019-06-20 23:26:08 -0400

    test display none

--- shaq/js/qs.slider.js
+++ shaq/js/qs.slider.js
@@ -33,8 +33,8 @@
               console.log($("#archiveBtn").is(":checked"));
           });
           $("#plants").on('change', function(){    // 2nd (A)
-              if ($("#ratingBtn").is(":checked")) $("#plantsSlide").removeClass("hide");
-              else $("#plantsSlide").addClass("hide");
+              if ($("#ratingBtn").is(":checked")) $("#plantsSlide").css('display','block');;
+              else $("#plantsSlide").css('display','none');;
           });
           $("#ratingBtn").on('change', function(){    // 2nd (A)
               if ($("#ratingBtn").is(":checked")) configPricing.PriceSHAQS += configPricing.PriceRATING;

287a700223c13646ea0dd5a1daceb057831e09a4
lav <l.venier@yoctu.com>
2020-02-29 13:20:16 -0500

    clean

--- yoctu/public/js/yoctu.js
+++ yoctu/public/js/yoctu.js
@@ -33,7 +33,7 @@ async function fetchProfile() {
 }
 
 function refresh(menuType) {
-    let menuList = ["solr", "nodered", "kafka", "couchdb", "welcome", "profile"];
+    let menuList = ["nodered", "welcome", "profile"];
     let cpt = 0;
     for (menu in menuList) {
         if ($("#" + menuList[menu])) {
@@ -45,21 +45,12 @@ function refresh(menuType) {
     $("#loader-container").removeClass("d-none");
     switch (menuType) {
         case menuList[0]:
-            fetchSolr();
-            break;
-        case menuList[1]:
             fetchNode();
             break;
-        case menuList[2]:
-            fetchKafka();
-            break;
-        case menuList[3]:
-            fetchCouchDB();
-            break;
-        case menuList[4]:
+        case menuList[1]:
             $("#welcome").removeClass("d-none");
             break;
-        case menuList[5]:
+        case menuList[2]:
             fetchProfileUser();
             break;
         default:
@@ -119,284 +110,15 @@ function displaynode(nodeProfile) {
     }, 500);
 }
 
-function displaycouchdb(couchdbProfile) {
-    $("#couchdb").find(".card-couchdb").remove();
-    $(".costcouchdb").addClass("d-none");
-    let costCouchdb = 0.00;
-    for (s in couchdbProfile) {
-        let well = $(".template-card-couchdb").clone();
-        well.appendTo("#couchdb");
-        well.removeClass("template-card-couchdb").addClass("card-couchdb");
-        well.find(".url").html("</div><div>Servers: <div>");
-        for (u in couchdbProfile[s].url) {
-            well.find(".url").append('<div class="text-center">' + couchdbProfile[s].url[u] + '</div>');
-        }
-        well.find(".user").html('User: <div class="text-center">' + couchdbProfile[s].user + '</div>');
-        well.find(".type").html('Type: <div class="text-center">' + couchdbProfile[s].type + '</div>');
-        well.find(".dbs").html("<div>Databases: <div>");
-        for (c in couchdbProfile[s].dbs) {
-            well.find(".dbs").append('<div class="text-center">' + c + '</div>');
-            if (couchdbProfile[s].type === "shared") costCouchdb += profile.price.shared.couchdb;
-            else costCouchdb += profile.price.dedicated.couchdb;
-        }
-        well.find(".ui").html();
-    }
-    $("#couchdbcost").html(costCouchdb.toFixed(2));
-    setTimeout(function () {
-        $("#loader-container").addClass("d-none");
-        $(".costcouchdb").removeClass("d-none");
-        $(".card-couchdb").removeClass("d-none");
-        $("#couchdb").removeClass("d-none");
-    }, 500);
-
-}
-
-function displaykafka(kafkaProfile) {
-    $("#kafka").find(".card-kafka").remove();
-    $(".costkafka").addClass("d-none");
-    let costKafka = 0.00;
-    for (s in kafkaProfile) {
-        let well = $(".template-card-kafka").clone();
-        well.appendTo("#kafka");
-        well.removeClass("template-card-kafka").addClass("card-kafka");
-        well.find(".url").html("</div><div>Brokers: <div>");
-        for (u in kafkaProfile[s].url) {
-            well.find(".url").append('<div class="text-center">' + kafkaProfile[s].url[u] + '</div>');
-        }
-        well.find(".user").html('User: <div class="text-center">' + kafkaProfile[s].user + '</div>');
-        well.find(".type").html('Type: <div class="text-center">' + kafkaProfile[s].type + '</div>');
-        well.find(".topics").html("<div>Topics: <div>");
-        for (c in kafkaProfile[s].topics) {
-            well.find(".topics").append('<div class="text-center">' + c + '</div>');
-            if (kafkaProfile[s].type === "shared") costKafka += profile.price.shared.kafka;
-            else costKafka += profile.price.dedicated.kafka;
-        }
-        well.find(".ui").html();
-    }
-    $("#kafkacost").html(costKafka.toFixed(2));
-    setTimeout(function () {
-        $("#loader-container").addClass("d-none");
-        $(".costkafka").removeClass("d-none");
-        $(".card-kafka").removeClass("d-none");
-        $("#kafka").removeClass("d-none");
-    }, 500);
-}
-
-function displaysolr(solrProfile) {
-    $("#solr").find(".card-solr").remove();
-    $(".costsolr").addClass("d-none");
-    let costSolr = 0.00;
-    for (s in solrProfile) {
-        let well = $(".template-card-solr").clone();
-        well.appendTo("#solr");
-        well.removeClass("template-card-solr").addClass("card-solr");
-        well.find(".url").html("</div><div>Servers: <div>");
-        for (u in solrProfile[s].url) {
-            well.find(".url").append('<div class="text-center">' + solrProfile[s].url[u] + '</div>');
-        }
-        well.find(".user").html('User: <div class="text-center">' + solrProfile[s].user + '</div>');
-        well.find(".type").html('Type: <div class="text-center">' + solrProfile[s].type + '</div>');
-        well.find(".collections").html("<div>Collections: <div>");
-        for (c in solrProfile[s].collections) {
-            well.find(".collections").append('<div class="text-center">' + c + '</div>');
-            if (solrProfile[s].type === "shared") costSolr += profile.price.shared.solr;
-            else costSolr += profile.price.dedicated.kafka;
-        }
-        well.find(".ui").html();
-    }
-    $("#solrcost").html(costSolr.toFixed(2));
-    setTimeout(function () {
-        $("#loader-container").addClass("d-none");
-        $(".costsolr").removeClass("d-none");
-        $(".card-solr").removeClass("d-none");
-        $("#solr").removeClass("d-none");
-    }, 500)
-}
-
 async function fetchProfileUser() {
     if (Object.keys(profile).length < 1) profile = await fetchProfile();
     displayprofile(profile);
-}
-
-async function fetchSolr() {
-    profile = await fetchProfile();
-    displaysolr(profile.solr);
 }
 
 async function fetchNode() {
     profile = await fetchProfile();
     displaynode(profile.nodered);
 }
-
-async function fetchKafka() {
-    profile = await fetchProfile();
-    displaykafka(profile.kafka);
-}
-
-async function fetchCouchDB() {
-    profile = await fetchProfile();
-    displaycouchdb(profile.couchdb);
-}
-
-$("#deleteCollection").on("click", function () {
-    $("#confirm-modal-yes").unbind();
-    $("#confirm-modal-yes").on("click", function () {
-        collection = $("#createname").val();
-        $("#QuestionModal").find(".modal-body").html("Deleting collection...");
-        server = profile.solr[s].url[0];
-        for (s in profile.solr)
-            for (c in profile.solr[s].collections)
-                if (c === collection) server = profile.solr[s].url[0];
-        $.ajax({
-            url: '/collection/' + idp + '/' + server + '/' + collection,
-            type: 'DELETE',
-            success: function (response) {
-                fetchSolr();
-                $("#QuestionModal").modal("hide");
-            },
-        });
-    });
-    let output = '<div class="form-group"><label>Collection : </label><select id="createname" class="form-control">';
-    for (s in profile.solr)
-        for (c in profile.solr[s].collections)
-            output += '<option>' + c + '</option>';
-    output += '</select></div>';
-    $("#QuestionModal").find(".modal-body").html(output);
-    $("#QuestionModal").find(".modal-title").html("Delete Collection");
-    $("#QuestionModal").modal("show");
-});
-
-$("#createCollection").on("click", function () {
-    $("#confirm-modal-yes").unbind();
-    $("#confirm-modal-yes").on("click", function () {
-        //var fd = new FormData();
-        //var files = $('#file')[0].files[0];
-        //fd.append('file', files);
-        let collection = $("#createname").val();
-        $("#QuestionModal").find(".modal-body").html("Creating collection...");
-        server = profile.solr[s].url[0];
-        $.ajax({
-            url: '/collection/' + idp + '/' + server + '/' + collection,
-            type: 'POST',
-            //   data: fd,
-            //   contentType: false,
-            //   processData: false,
-            success: function (response) {
-                fetchSolr();
-                $("#QuestionModal").modal("hide");
-            },
-            statusCode: {
-                406: function (response) {
-                    $("#QuestionModal").modal("hide");
-                    $("#InformationModalBody").html("Collection already Exists !");
-                    $("#InformationModal").modal("show");
-                },
-            }
-        });
-    });
-    $("#QuestionModal").find(".modal-body").html('<div class="form-group"><label>Name : </label><input id="createname" class="form-control"></input></div>');
-    $("#QuestionModal").find(".modal-body").append('<div class="form-group"><label>Type : </label><select id="createtype" class="form-control"><option>Shared</option></select></div>');
-    /*$("#QuestionModal").find(".modal-body").append('<div class="form-group"><label>Config Set : </label>\
-    <form method="post" action="" enctype="multipart/form-data" id="myform">\
-    <div><input type="file" id="file" name="file" /></div></form> ');*/
-    $("#QuestionModal").find(".modal-title").html("Create Collection");
-    $("#QuestionModal").modal("show");
-});
-
-$("#deleteTopic").on("click", function () {
-    $("#confirm-modal-yes").unbind();
-    $("#confirm-modal-yes").on("click", function () {
-        let topic = $("#createname").val();
-        $("#QuestionModal").find(".modal-body").html("Deleting Database...");
-        server = profile.kafka[s].url[0];
-        for (s in profile.kafka)
-            for (c in profile.kafka[s].topics)
-                if (c === topic) server = profile.kafka[s].url[0];
-        $.ajax({
-            url: '/topic/' + idp + '/' + server + '/' + topic,
-            type: 'DELETE',
-            success: function (response) {
-                fetchKafka();
-                $("#QuestionModal").modal("hide");
-            },
-        });
-    });
-    let output = '<div class="form-group"><label>Topic : </label><select id="createname" class="form-control">';
-    for (s in profile.kafka)
-        for (c in profile.kafka[s].topics)
-            output += '<option>' + c + '</option>';
-    output += '</select></div>';
-    $("#QuestionModal").find(".modal-body").html(output);
-    $("#QuestionModal").find(".modal-title").html("Delete Topic");
-    $("#QuestionModal").modal("show");
-});
-
-$("#createTopic").on("click", function () {
-    $("#confirm-modal-yes").unbind();
-    $("#confirm-modal-yes").on("click", function () {
-        server = profile.kafka[s].url[0];
-        $.ajax({
-            url: '/topic/' + idp + '/' + server + '/' + $("#createname").val(),
-            type: 'POST',
-            success: function (response) {
-                fetchKafka();
-                $("#QuestionModal").modal("hide");
-            },
-        });
-    });
-    $("#QuestionModal").find(".modal-body").html('<div class="form-group"><label>Name : </label><input id="createname" class="form-control"></input></div>');
-    $("#QuestionModal").find(".modal-body").append('<div class="form-group"><label>Type : </label><select id="createtype" class="form-control"><option>Shared</option></select></div>');
-    $("#QuestionModal").find(".modal-body").append('<div class="form-group"><label>Partitions : </label><select id="createpartition" class="form-control"><option>1</option></select></div>');
-    $("#QuestionModal").find(".modal-title").html("Create Topic");
-    $("#QuestionModal").modal("show");
-});
-
-$("#deleteDb").on("click", function () {
-    $("#confirm-modal-yes").unbind();
-    $("#confirm-modal-yes").on("click", function () {
-        let db = $("#createname").val();
-        $("#QuestionModal").find(".modal-body").html("Deleting Database...");
-        server = profile.couchdb[s].url[0];
-        for (s in profile.couchdb)
-            for (c in profile.couchdb[s].dbs)
-                if (c === db) server = profile.couchdb[s].url[0];
-        $.ajax({
-            url: '/db/' + idp + '/' + server + '/' + db,
-            type: 'DELETE',
-            success: function (response) {
-                fetchCouchDB();
-                $("#QuestionModal").modal("hide");
-            },
-        });
-    });
-    let output = '<div class="form-group"><label>Database : </label><select id="createname" class="form-control">';
-    for (s in profile.couchdb)
-        for (c in profile.couchdb[s].dbs)
-            output += '<option>' + c + '</option>';
-    output += '</select></div>';
-    $("#QuestionModal").find(".modal-body").html(output);
-    $("#QuestionModal").find(".modal-title").html("Delete Database");
-    $("#QuestionModal").modal("show");
-});
-
-$("#createDb").on("click", function () {
-    $("#confirm-modal-yes").unbind();
-    $("#confirm-modal-yes").on("click", function () {
-        server = profile.couchdb[s].url[0];
-        $.ajax({
-            url: '/db/' + idp + '/' + server + '/' + $("#createname").val(),
-            type: 'POST',
-            success: function (response) {
-                fetchCouchDB();
-                $("#QuestionModal").modal("hide");
-            },
-        });
-    });
-    $("#QuestionModal").find(".modal-body").html('<div class="form-group"><label>Name : </label><input id="createname" class="form-control"></input></div>');
-    $("#QuestionModal").find(".modal-body").append('<div class="form-group"><label>Type : </label><select id="createtype" class="form-control"><option>Shared</option></select></div>');
-    $("#QuestionModal").find(".modal-title").html("Create Database");
-    $("#QuestionModal").modal("show");
-});
 
 $(document).ready(function () {
 
